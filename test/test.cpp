e69c8865fcc9fb407f2191dbd4230a691a6c8d81
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-11-14 12:21:16 +0500

    Add unit tests for testing pop_back function

--- test/test.cpp
+++ test/test.cpp
@@ -6,11 +6,32 @@ TEST_CASE("test deque.push_back function")
     SECTION("should return expected value")
     {
         Deque deque;
-        size_t expected_size = rand() % 10;
+        size_t expected_size = 10;
 
         for (size_t i = 0; i < expected_size; i++)
             deque.push_back(i + 1);
 
+        REQUIRE(deque.get_size() == expected_size);
+        for (size_t i = 0; i < expected_size; i++)
+            REQUIRE(deque[i] == i + 1);
+    }
+}
+
+TEST_CASE("test deque.pop_back function")
+{
+    SECTION("should return expected value")
+    {
+        Deque deque;
+        size_t size = 10;
+
+        for (size_t i = 0; i < size; i++)
+            deque.push_back(i + 1);
+
+        for (size_t i = 0;  i < 3; i++)
+            deque.pop_back();
+
+        size_t expected_size = 7;
+        
         REQUIRE(deque.get_size() == expected_size);
         for (size_t i = 0; i < expected_size; i++)
             REQUIRE(deque[i] == i + 1);
