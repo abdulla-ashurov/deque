abbb111ee9dcb90c19d3d6d239ac2bc8a7cfd5ba
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-11-15 13:04:32 +0500

    Add unit tests for checking push_front and push_back functions

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,7 @@
 
 TEST_CASE("test deque.push_back function")
 {
-    SECTION("should return expected value")
+    SECTION("should return expected values")
     {
         Deque<int> deque;
         size_t expected_size = 10;
@@ -17,9 +17,62 @@ TEST_CASE("test deque.push_back function")
     }
 }
 
+TEST_CASE("test deque.push_front function")
+{
+    SECTION("should return expected values")
+    {
+        Deque<int> deque;
+        size_t expected_size = 10;
+
+        for (size_t i = 0; i < expected_size; i++)
+            deque.push_front(i + 1);
+
+        REQUIRE(deque.get_size() == expected_size);
+        for (size_t i = 0; i < expected_size; i++)
+            REQUIRE(deque[i] == expected_size - i);
+    }
+}
+
+TEST_CASE("test deque.push_back and push_front functions")
+{
+    SECTION("should return expected values")
+    {
+        Deque<int> deque;
+        size_t expected_size = 10;
+        int expected_values[] = {6, 7, 8, 9, 10, 1, 2, 3, 4, 5};
+
+        for (size_t i = 0; i < expected_size / 2; i++)
+            deque.push_back(i + 1);
+        
+        for (size_t i = 0; i < expected_size / 2; i++)
+            deque.push_front(expected_size - i);
+        
+        REQUIRE(deque.get_size() == expected_size);
+        for (size_t i = 0; i < expected_size; i++)
+            REQUIRE(deque[i] == expected_values[i]);
+    }
+
+    SECTION("should return expected values")
+    {
+        Deque<int> deque;
+        size_t expected_size = 10;
+        int expected_values[] = {5, 4, 3, 2, 1, 10, 9, 8, 7, 6};
+
+        for (size_t i = 0; i < expected_size / 2; i++)
+            deque.push_front(i + 1);
+        
+        for (size_t i = 0; i < expected_size / 2; i++)
+            deque.push_back(expected_size - i);
+        
+        REQUIRE(deque.get_size() == expected_size);
+        for (size_t i = 0; i < expected_size; i++)
+            REQUIRE(deque[i] == expected_values[i]);
+    }
+}
+
 TEST_CASE("test deque.pop_back function")
 {
-    SECTION("should return expected value")
+    SECTION("should return expected values")
     {
         Deque<int> deque;
         size_t size = 10;
