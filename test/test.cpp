14a921752e9bab1fbccac10d385fd2ffd637ac81
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-11-20 19:48:58 +0500

    Add unit tests for testing array class

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,147 @@
 #include "catch_amalgamated.hpp"
 #include "../include/deque.hpp"
 
+////////////////////////////////////////////////////////////////////////////
+/////////////////// TEST CASES FOR TESTING ARRAY_HPP ///////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+TEST_CASE("test array.push_front function")
+{
+    SECTION("should return expected values")
+    {
+        Array<int> array;
+        size_t expected_size = array.get_max_size();
+
+        for (size_t i = 0; i < expected_size; i++)
+            array.push_front(i + 1);
+        
+        REQUIRE(array.get_size() == expected_size);
+        for (size_t i = 0; i < expected_size; i++)
+            REQUIRE(array[i] == expected_size - i);
+    }
+
+    SECTION("should return exception")
+    {
+        Array<int> array;
+        size_t expected_size = array.get_max_size();
+
+        for (size_t i = 0; i < expected_size; i++)
+            array.push_front(i + 1);
+
+        REQUIRE(array.get_size() == expected_size);
+
+        // we cannot add element, because array is full
+        REQUIRE_THROWS(array.push_front(rand()));
+    }
+}
+
+TEST_CASE("test array.push_back function")
+{
+    SECTION("should return expected values")
+    {
+        Array<int> array;
+        size_t expected_size = array.get_max_size();
+
+        for (size_t i = 0; i < expected_size; i++)
+            array.push_back(i + 1);
+        
+        REQUIRE(array.get_size() == expected_size);
+        for (size_t i = 0; i < expected_size; i++)
+            REQUIRE(array[i] == i + 1);
+    }
+
+    SECTION("should return exception")
+    {
+        Array<int> array;
+        size_t expected_size = array.get_max_size();
+
+        for (size_t i = 0; i < expected_size; i++)
+            array.push_back(i + 1);
+
+        REQUIRE(array.get_size() == expected_size);
+
+        // we cannot add element, because array is full
+        REQUIRE_THROWS(array.push_back(rand()));
+    }
+}
+
+TEST_CASE("test array.pop_front function")
+{
+    SECTION("should return expected values")
+    {
+        Array<int> array;
+        for (size_t i = 0; i < array.get_max_size(); i++)
+            array.push_front(i + 1);
+        
+        REQUIRE(array.get_size() == array.get_max_size());
+        
+        for (size_t i = 0; i < 2; i++)
+            array.pop_front();
+        
+        size_t expected_size = 2;
+
+        REQUIRE(array.get_size() == expected_size);
+        for (size_t i = 0; i < expected_size; i++)
+            REQUIRE(array[i] == expected_size - i);
+    }
+
+    SECTION("should return exception")
+    {
+        Array<int> array;
+
+        // we cannot pop_front array, because is empty
+        REQUIRE_THROWS(array.pop_front());
+    }
+}
+
+TEST_CASE("test array.pop_back function")
+{
+    SECTION("should return expected values")
+    {
+        Array<int> array;
+        for (size_t i = 0; i < array.get_max_size(); i++)
+            array.push_back(i + 1);
+        
+        REQUIRE(array.get_size() == array.get_max_size());
+        
+        for (size_t i = 0; i < 2; i++)
+            array.pop_back();
+        
+        size_t expected_size = 2;
+
+        REQUIRE(array.get_size() == expected_size);
+        for (size_t i = 0; i < expected_size; i++)
+            REQUIRE(array[i] == i + 1);
+    }
+
+    SECTION("should return exception")
+    {
+        Array<int> array;
+
+        // we cannot pop_back array, because is empty
+        REQUIRE_THROWS(array.pop_back());
+    }
+}
+
+TEST_CASE("test array.clear function")
+{
+    SECTION("should return empty array")
+    {
+        Array<int> array;
+        for (size_t i = 0; i < array.get_max_size(); i++)
+            array.push_back(i + 1);
+
+        size_t expected_size = 0;
+        array.clear();
+        
+        REQUIRE(array.get_size() == expected_size);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////
+/////////////////// TEST CASES FOR TESTING DEQUE_HPP ///////////////////////
+////////////////////////////////////////////////////////////////////////////
+
 TEST_CASE("test deque.push_front function")
 {
     SECTION("should return expected values")
