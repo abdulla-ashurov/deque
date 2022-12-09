7abcc5e8115d6692f40fc1754961ee113d29cd41
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-12-10 00:30:52 +0500

    Add more unit tests for Array class

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,28 @@
+#include <string>
 #include "catch_amalgamated.hpp"
 #include "../include/deque.hpp"
+
+std::string gen_random_str(int len)
+{
+    std::string s;
+    static const char characters[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < len; i++)
+    {
+        s += characters[rand() % (sizeof(characters) - 1)];
+    }
+
+    return s;
+}
+
+void init_str_array(std::string *array, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+        array[i] = gen_random_str(10);
+}
 
 ////////////////////////////////////////////////////////////////////////////
 /////////////////// TEST CASES FOR TESTING ARRAY_HPP ///////////////////////
@@ -14,10 +37,26 @@ TEST_CASE("test array.push_front function")
 
         for (size_t i = 0; i < expected_size; i++)
             array.push_front(i + 1);
-        
+
         REQUIRE(array.size() == expected_size);
         for (size_t i = 0; i < expected_size; i++)
             REQUIRE(array[i] == expected_size - i);
+    }
+
+    SECTION("should return expected values")
+    {
+        Array<std::string> array;
+        size_t expected_size = array.get_max_size();
+
+        std::string expected_values[expected_size];
+        init_str_array(expected_values, expected_size);
+
+        for (size_t i = 0; i < expected_size; i++)
+            array.push_front(expected_values[i]);
+
+        REQUIRE(array.size() == expected_size);
+        for (size_t i = 0; i < expected_size; i++)
+            REQUIRE(array[i] == expected_values[expected_size - i - 1]);
     }
 }
 
@@ -30,10 +69,26 @@ TEST_CASE("test array.push_back function")
 
         for (size_t i = 0; i < expected_size; i++)
             array.push_back(i + 1);
-        
+
         REQUIRE(array.size() == expected_size);
         for (size_t i = 0; i < expected_size; i++)
             REQUIRE(array[i] == i + 1);
+    }
+
+    SECTION("should return expected values")
+    {
+        Array<std::string> array;
+        size_t expected_size = array.get_max_size();
+
+        std::string expected_values[expected_size];
+        init_str_array(expected_values, expected_size);
+
+        for (size_t i = 0; i < expected_size; i++)
+            array.push_back(expected_values[i]);
+
+        REQUIRE(array.size() == expected_size);
+        for (size_t i = 0; i < expected_size; i++)
+            REQUIRE(array[i] == expected_values[i]);
     }
 }
 
@@ -44,17 +99,38 @@ TEST_CASE("test array.pop_front function")
         Array<int> array;
         for (size_t i = 0; i < array.get_max_size(); i++)
             array.push_front(i + 1);
-        
+
         REQUIRE(array.size() == array.get_max_size());
-        
+
         for (size_t i = 0; i < 2; i++)
             array.pop_front();
-        
+
         size_t expected_size = 2;
 
         REQUIRE(array.size() == expected_size);
         for (size_t i = 0; i < expected_size; i++)
             REQUIRE(array[i] == expected_size - i);
+    }
+
+    SECTION("should return expected values")
+    {
+        Array<std::string> array;
+
+        std::string expected_values[array.get_max_size()];
+        init_str_array(expected_values, array.get_max_size());
+
+        for (size_t i = 0; i < array.get_max_size(); i++)
+            array.push_front(expected_values[i]);
+
+        REQUIRE(array.size() == array.get_max_size());
+
+        for (size_t i = 0; i < 2; i++)
+            array.pop_front();
+
+        size_t expected_size = 2;
+        REQUIRE(array.size() == expected_size);
+        for (size_t i = 0; i < expected_size; i++)
+            REQUIRE(array[i] == expected_values[expected_size - i - 1]);
     }
 }
 
@@ -65,17 +141,38 @@ TEST_CASE("test array.pop_back function")
         Array<int> array;
         for (size_t i = 0; i < array.get_max_size(); i++)
             array.push_back(i + 1);
-        
+
         REQUIRE(array.size() == array.get_max_size());
-        
+
         for (size_t i = 0; i < 2; i++)
             array.pop_back();
-        
+
         size_t expected_size = 2;
 
         REQUIRE(array.size() == expected_size);
         for (size_t i = 0; i < expected_size; i++)
             REQUIRE(array[i] == i + 1);
+    }
+
+    SECTION("should return expected values")
+    {
+        Array<std::string> array;
+
+        std::string expected_values[array.get_max_size()];
+        init_str_array(expected_values, array.get_max_size());
+
+        for (size_t i = 0; i < array.get_max_size(); i++)
+            array.push_back(expected_values[i]);
+
+        REQUIRE(array.size() == array.get_max_size());
+
+        for (size_t i = 0; i < 2; i++)
+            array.pop_back();
+
+        size_t expected_size = 2;
+        REQUIRE(array.size() == expected_size);
+        for (size_t i = 0; i < expected_size; i++)
+            REQUIRE(array[i] == expected_values[i]);
     }
 }
 
@@ -89,7 +186,19 @@ TEST_CASE("test array.clear function")
 
         size_t expected_size = 0;
         array.clear();
-        
+
+        REQUIRE(array.size() == expected_size);
+    }
+
+    SECTION("should return empty array")
+    {
+        Array<std::string> array;
+        for (size_t i = 0; i < array.get_max_size(); i++)
+            array.push_back(gen_random_str(10));
+
+        size_t expected_size = 0;
+        array.clear();
+
         REQUIRE(array.size() == expected_size);
     }
 }
@@ -140,10 +249,10 @@ TEST_CASE("test deque.push_back and push_front functions")
 
         for (size_t i = 0; i < expected_size / 2; i++)
             deque.push_back(i + 1);
-        
+
         for (size_t i = 0; i < expected_size / 2; i++)
             deque.push_front(expected_size - i);
-        
+
         REQUIRE(deque.get_size() == expected_size);
         for (size_t i = 0; i < expected_size; i++)
             REQUIRE(deque[i] == expected_values[i]);
@@ -157,10 +266,10 @@ TEST_CASE("test deque.push_back and push_front functions")
 
         for (size_t i = 0; i < expected_size / 2; i++)
             deque.push_front(i + 1);
-        
+
         for (size_t i = 0; i < expected_size / 2; i++)
             deque.push_back(expected_size - i);
-        
+
         REQUIRE(deque.get_size() == expected_size);
         for (size_t i = 0; i < expected_size; i++)
             REQUIRE(deque[i] == expected_values[i]);
@@ -181,7 +290,7 @@ TEST_CASE("test deque.pop_front function")
             deque.pop_front();
 
         size_t expected_size = 7;
-        
+
         REQUIRE(deque.get_size() == expected_size);
         for (size_t i = 0; i < expected_size; i++)
             REQUIRE(deque[i] == expected_size - i);
@@ -204,11 +313,11 @@ TEST_CASE("test deque.pop_back function")
         for (size_t i = 0; i < size; i++)
             deque.push_back(i + 1);
 
-        for (size_t i = 0;  i < 3; i++)
+        for (size_t i = 0; i < 3; i++)
             deque.pop_back();
 
         size_t expected_size = 7;
-        
+
         REQUIRE(deque.get_size() == expected_size);
         for (size_t i = 0; i < expected_size; i++)
             REQUIRE(deque[i] == i + 1);
