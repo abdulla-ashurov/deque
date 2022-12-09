7f4ea0361001b6c8b007e29e799bed994f3cb216
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-12-10 00:33:32 +0500

    Refactor code in array.hpp

--- include/array.hpp
+++ include/array.hpp
@@ -9,14 +9,14 @@ template <class T>
 class Array
 {
 public:
-    Array() : array{new T[max_size]}, _size{0} {}
+    Array() : m_array{new T[max_size]}, m_size{0} {}
     Array(const Array &other) = delete;
 
-    bool full() const { return _size == max_size; }
+    bool full() const { return m_size == max_size; }
 
-    bool empty() const { return _size == 0; }
+    bool empty() const { return m_size == 0; }
 
-    size_t size() const { return _size; }
+    size_t size() const { return m_size; }
 
     static size_t get_max_size() { return max_size; }
 
@@ -24,54 +24,54 @@ public:
     {
         assert_m(full(), "array overflow");
 
-        for (int i = _size - 1; i >= 0; i--)
-            std::swap(array[i], array[i + 1]);
+        for (int i = m_size - 1; i >= 0; i--)
+            std::swap(m_array[i], m_array[i + 1]);
 
-        array[0] = value;
-        _size++;
+        m_array[0] = value;
+        m_size++;
     }
 
     void push_back(const T &value)
     {
         assert_m(full(), "array overflow");
 
-        array[_size] = value;
-        _size++;
+        m_array[m_size] = value;
+        m_size++;
     }
 
     void pop_front()
     {
         assert_m(empty(), "array is empty");
 
-        for (size_t i = 0; i < _size - 1; i++)
-            std::swap(array[i], array[i + 1]);
+        for (size_t i = 0; i < m_size - 1; i++)
+            std::swap(m_array[i], m_array[i + 1]);
 
-        array[_size - 1] = T();
-        _size--;
+        m_array[m_size - 1] = T();
+        m_size--;
     }
 
     void pop_back()
     {
         assert_m(empty(), "array is empty");
 
-        array[_size - 1] = T();
-        _size--;
+        m_array[m_size - 1] = T();
+        m_size--;
     }
 
     void clear()
     {
-        delete[] array;
-        array = nullptr;
-        _size = 0;
+        delete[] m_array;
+        m_array = nullptr;
+        m_size = 0;
     }
 
     Array& operator=(const Array &other) = delete;
 
     T &operator[](const size_t index)
     {
-        assert_m(index >= _size, "index out of range");
+        assert_m(index >= m_size, "index out of range");
         
-        return array[index];
+        return m_array[index];
     }
 
     ~Array()
@@ -80,8 +80,8 @@ public:
     }
 
 private:
-    T *array;
-    size_t _size;
+    T *m_array;
+    size_t m_size;
     const static size_t max_size = 4;
 };
 
